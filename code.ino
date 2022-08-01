830d97432ebef53da355a49847ee34886e8f65d7
Arsh2607 <79957801+Arsh2607@users.noreply.github.com>
2022-08-02 04:25:37 +1000

    Add files via upload

---
+++ code.ino
@@ -0,0 +1,46 @@
+
+#include "DHT.h"
+#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("CLEARDATA");
+  Serial.println("LABEL, time, temp");
+   Serial.println("RESETTIMER");
+  
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float temp = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(temp) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  
+  float hic = dht.computeHeatIndex(temp, h, false);
+
+ 
+ 
+  Serial.println(temp);
+  Serial.print("DATA, TIME, TIMER, DATE,");
+  Serial.print(temp);
+ 
+  
+}
